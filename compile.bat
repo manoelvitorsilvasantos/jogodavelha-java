c21604a8963d451fb973c8dea25047893515a629
Manoel Vitor <mvictordossantos.limite@gmail.com>
2022-01-09 22:56:27 -0300

    Add files via upload

---
+++ compile.bat
@@ -0,0 +1,8 @@
+@echo off
+title Jogo da velha
+echo Iniciando jogo...
+color 1f
+cd src
+javac *.java
+move *.class ..\classes\
+pause
---
+++ run.bat
@@ -0,0 +1,7 @@
+@echo off
+title Jogo da velha
+echo Iniciando jogo...
+color 1f
+cd classes
+java Game
+pause
